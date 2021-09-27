d0dffadde49c57b32722aa10dada9e0035302847
Martin Teigen <martin@teigen.biz>
2021-09-27 02:33:14 +0200

    website info update

--- js/IndexModule.js
+++ js/IndexModule.js
@@ -1,9 +1,10 @@
 const IndexModule = function () {
 
     const websites = [
-        {name: "MoreGames", info: "MoreGames is a website created for my game developer studio. Here you can find info about the games I produce, as well as social media accounts.", link: "https://moregames.no/"},
-        {name: "Cinematic", info: "Cinematic began as a school assignment at the beginning of the 1st semester of college. I chose to continue working and decided to create a platform similar to Netflix.<br><br> This website was developed at an early stage where I only had some knowledge of HTML, CSS and Javascript from high school.", link: "https://mtech.no/cinematic/index.html"},
-        {name: "Poseidon", info: "Poseidon is an assignment from the exam in the 1st semester. It was only allowed to use HTML and CSS, no Javascript. It's made by a group of 5, where I was responsible for <i>yachts.html, islands.html</i> and <i>movies.html</i>.<br><br> For this exam, we had no requirements for mobile optimization, so there may be deviations when testing on smaller screens.", link: "https://mtech.no/poseidon/index.html"}
+        {name: "MoreGames", info: "<i>MoreGames</i> is a website created for my game developer studio. Here you can find info about the games I produce, as well as social media accounts.", link: "https://moregames.no/"},
+        {name: "Gyldne Pizza", info: "<i>Gylne Pizza</i> is an assignment from the exam in the 2nd semester of college. We were a group of 6 and the focus was universal design and ease of use. I was responsible for meny.html. <br><br><b>There was no requirement for a backend, but we created a type of login service that I was involved in developing.</b>", link: "https://mtech.no/gyldne_pizza/"},
+        {name: "Cinematic", info: "<i>Cinematic</i> began as a school assignment at the beginning of the 1st semester of college. I chose to continue working and decided to create a platform similar to Netflix. <br><br><b>This website was developed at an early stage where I only had some knowledge of HTML, CSS and Javascript from high school.</b>", link: "https://mtech.no/cinematic/index.html"},
+        {name: "Poseidon", info: "<i>Poseidon</i> is an assignment from the exam in the 1st semester of college. Only HTML and CSS was allowed, no Javascript. It's made by a group of 5, where I was responsible for <i>yachts.html, islands.html</i> and <i>movies.html</i>.<br><br> <b>For this exam, we had no requirements for mobile optimization, therefore it's not suitable for smaller devies.</b>", link: "https://mtech.no/poseidon/index.html"}
     ];
 
     const printAllWebsites = () => websites;
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import IndexModule from "./IndexModule.js";
 let websites = "";
 const websitesContainer = document.querySelector("#websites-container");
 
-IndexModule.printAllWebsites().forEach(site => {
+IndexModule.printAllWebsites().forEach(site => {  
     websites += `
     
     <div class="card column is-3-desktop is-4-tablet m-5 p-0">
@@ -12,7 +12,7 @@ IndexModule.printAllWebsites().forEach(site => {
             <!-- Image -->
             <div class="card-image">
                 <figure class="image">
-                    <img class="website-image" src="../images/websites/${site.name}.png" alt="${site.name} logo">
+                    <img class="website-image" src="../images/websites/${site.name.replace(" ", "")}.png" alt="${site.name} logo">
                 </figure>
             </div>
 
